07a9571e84c8810f6ffc12d62c80f96664c31ee6
EAST-SOYA-DEV <codingwithsoya@gmail.com>
2024-09-20 09:46:36 +0300

    Configuring Client-side auth and implementing Login page.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,18 @@
+using Blazored.LocalStorage;
 using CSMessagingApp.Client;
+using CSMessagingApp.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
+
+builder.Services.AddAuthorizationCore();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
