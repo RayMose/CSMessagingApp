578932220de02d95dcba0e2bd867a0b037aebb3a
EAST-SOYA-DEV <codingwithsoya@gmail.com>
2024-09-20 09:36:36 +0300

    Implementing client-side Auth service.

---
+++ Client/Services/AuthService.cs
@@ -0,0 +1,41 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace CSMessagingApp.Client.Services
+{
+    public class AuthService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthService(HttpClient httpClient, AuthenticationStateProvider authStateProvider, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _authStateProvider = authStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/auth/login", new { Username = username, Password = password });
+            if (!response.IsSuccessStatusCode)
+                return false;
+
+            var result = await response.Content.ReadFromJsonAsync<LoginResult>();
+            await _localStorage.SetItemAsync("authToken", result.Token);
+            ((ApiAuthenticationStateProvider)_authStateProvider).MarkUserAsAuthenticated(username);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.Token);
+            return true;
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _localStorage.RemoveItemAsync("authToken");
+            ((ApiAuthenticationStateProvider)_authStateProvider).MarkUserAsLoggedOut();
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+        }
+    }
+}
