eb2a8e1151c3404c09cb7eefb5cb7007e8547bf2
EAST-SOYA-DEV <codingwithsoya@gmail.com>
2024-09-20 10:28:53 +0300

    Implementing the Message.cs model class.

---
+++ Server/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace CSMessagingApp.Server.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime Timestamp { get; set; }
+        public MessageStatus Status { get; set; }
+        public int? AssignedAgentId { get; set; }
+        public Agent AssignedAgent { get; set; }
+    }
+
+    public enum MessageStatus
+    {
+        Pending,
+        InProgress,
+        Resolved
+    }
+}
