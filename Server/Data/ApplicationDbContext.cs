f71ca79dd2f11d13ec9e76c655de5b834b91c6c3
EAST-SOYA-DEV <codingwithsoya@gmail.com>
2024-09-20 10:41:24 +0300

    Implementing the ApplicationDbContext.cs class.

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CSMessagingApp.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSMessagingApp.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Agent> Agents { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.AssignedAgent)
+                .WithMany()
+                .HasForeignKey(m => m.AssignedAgentId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
