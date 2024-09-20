8d215ca52d82ccaacfde04bde33f76e3e59acd6e
EAST-SOYA-DEV <codingwithsoya@gmail.com>
2024-09-20 09:26:37 +0300

    Implementing the Auth Controller.

---
+++ Server/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSMessagingApp.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var token = await _authService.AuthenticateAsync(loginDto.Username, loginDto.Password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(new { Token = token });
+        }
+    }
+}
